b090c5189a4d74ac38cf108a217bb5f31906a3bd
Andrew Macheret <andrew.macheret@gmail.com>
2017-01-27 14:57:27 -0800

    Fixes for better error handling, consistency, and ease of use

--- aws-teardown.sh
+++ aws-teardown.sh
@@ -1,11 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 source ./config/variables
-source .env
 
-aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonSNSFullAccess
-aws iam delete-role --role-name $ROLE_NAME
-aws sns delete-topic --topic-arn $SNS_TOPIC_ARN
-aws lambda delete-function --function-name $LAMBDA_FUNCTION_NAME
+if [ -f .env ]; then
+  source .env
+fi
 
-rm .env
+echo "Detaching role policy AmazonSNSFullAccess from role $ROLE_NAME"
+aws iam detach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AmazonSNSFullAccess || true
+
+echo "Deleting role $ROLE_NAME"
+aws iam delete-role --role-name "$ROLE_NAME" || true
+
+if [ ! -z "$AWS_SNS_TOPIC_ARN" ]; then
+  echo "Deleting topic $AWS_SNS_TOPIC_ARN"
+  aws sns delete-topic --topic-arn "$AWS_SNS_TOPIC_ARN" || true
+fi
+
+if [ ! -z "$LAMBDA_FUNCTION_NAME" ]; then
+  echo "Deleting lambda function $LAMBDA_FUNCTION_NAME"
+  aws lambda delete-function --function-name "$LAMBDA_FUNCTION_NAME" || true
+fi
+
+rm -f .env
