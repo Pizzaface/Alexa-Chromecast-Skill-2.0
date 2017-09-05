ace599241e286b59c4ddc5a26c5e07661dc895cb
Rafi Ahmad <rafiahmad@gmail.com>
2017-09-05 16:45:59 -0700

    Changes for fixing Chromecast Blue icon on TV problem

--- src/local/ChromecastSkill.py
+++ src/local/ChromecastSkill.py
@@ -41,7 +41,7 @@ class Skill():
         print('Volume command sent to Chromecast. Set to {}.'.format(volume_normalized))
 
     def play_video(self, data, cast, mc):
-        url = subprocess.check_output("youtube-dl -g --no-check-certificate -- " + data['videoId'], shell=True)
+        url = subprocess.check_output("youtube-dl -g --no-check-certificate -f best -- " + data['videoId'], shell=True)
         mc.play_media(url, 'video/mp4')
         print('video sent to chromecast: {}'.format(url))
 
